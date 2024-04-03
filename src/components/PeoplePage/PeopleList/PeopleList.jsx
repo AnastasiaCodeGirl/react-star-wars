450047a7c6be3ad65ffb32721debf9fd607d61cb
Anastasiia Shumeiko <anastasiia.shumeiko@macaw.nl>
2024-04-03 13:29:38 +0200

    basic styling

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -1,13 +1,15 @@
 import styles from './PeopleList.module.css';
 
-const PeopleList = () => {
+const PeopleList = ({people}) => {
     return (
         
-          <ul>
+          <ul className={styles.list__container}>
           {people.map(({ id, name, img }) => {
-             <li key={id}>
-                <img src={img} alt={name} />
-                <p>{name}</p>
+             return <li className={styles.list__item} key={id}>
+                <a href="#">
+                    <img className={styles.person__photo} src={img} alt={name} />
+                    <p>{name}</p>
+                </a>
                 </li>; // Added return statement
           })}
         </ul>
